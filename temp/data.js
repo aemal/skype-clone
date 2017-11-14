c04f533c105a0b09bc73a58e9de511a5cd446965
hassoun86 <husseinfarhat1@hotmail.com>
2017-11-14 16:13:00 +0100

    data.js file added

---
+++ temp/data.js
@@ -0,0 +1,122 @@
+db.users.insert
+([{
+	emailAddress: "john@gmail.com",
+	password: "john123",
+	dateOfBirth: "14/3/1987",
+	profile:{
+		firstName: "john",
+		lastName: "balout",
+		Avatar: "image.jpg"
+	},
+	status:{
+		lastSeen: "2 min",
+	    active: true
+	},
+	socialMediaSignUp:{},
+	contacts:{
+		list: ["jad"],
+		requested: ["sam"],
+		pending: ["rami"],
+		blocked: ["bad"]
+	}
+},{
+	emailAddress: "john@gmail.com",
+	password: "john123",
+	dateOfBirth: "14/3/1987",
+	profile:{
+		firstName: "john",
+		lastName: "balout",
+		Avatar: "image.jpg"
+	},
+	status:{
+		lastSeen: "2 min",
+	    active: true
+	},
+	socialMediaSignUp:{},
+	contacts:{
+		list: ["jad"],
+		requested: ["sam"],
+		pending: ["rami"],
+		blocked: ["bad"]
+	}
+},{
+	emailAddress: "john@gmail.com",
+	password: "john123",
+	dateOfBirth: "14/3/1987",
+	profile:{
+		firstName: "john",
+		lastName: "balout",
+		Avatar: "image.jpg"
+	},
+	status:{
+		lastSeen: "2 min",
+	    active: true
+	},
+	socialMediaSignUp:{},
+	contacts:{
+		list: ["jad"],
+		requested: ["sam"],
+		pending: ["rami"],
+		blocked: ["bad"]
+	}
+},{
+	emailAddress: "john@gmail.com",
+	password: "john123",
+	dateOfBirth: "14/3/1987",
+	profile:{
+		firstName: "john",
+		lastName: "balout",
+		Avatar: "image.jpg"
+	},
+	status:{
+		lastSeen: "2 min",
+	    active: true
+	},
+	socialMediaSignUp:{},
+	contacts:{
+		list: ["jad"],
+		requested: ["sam"],
+		pending: ["rami"],
+		blocked: ["bad"]
+	}
+},{
+	emailAddress: "john@gmail.com",
+	password: "john123",
+	dateOfBirth: "14/3/1987",
+	profile:{
+		firstName: "john",
+		lastName: "balout",
+		Avatar: "image.jpg"
+	},
+	status:{
+		lastSeen: "2 min",
+	    active: true
+	},
+	socialMediaSignUp:{},
+	contacts:{
+		list: ["jad"],
+		requested: ["sam"],
+		pending: ["rami"],
+		blocked: ["bad"]
+	}
+},{
+	emailAddress: "john@gmail.com",
+	password: "john123",
+	dateOfBirth: "14/3/1987",
+	profile:{
+		firstName: "john",
+		lastName: "balout",
+		Avatar: "image.jpg"
+	},
+	status:{
+		lastSeen: "2 min",
+	    active: true
+	},
+	socialMediaSignUp:{},
+	contacts:{
+		list: ["jad"],
+		requested: ["sam"],
+		pending: ["rami"],
+		blocked: ["bad"]
+	}
+}]);
