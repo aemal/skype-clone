d3b63b4d78f4121982f5227620721be6b0fd857e
Aemal Sayer <aemalsayer@aemals-air.intern>
2017-11-14 14:37:33 +0100

    added package.json

---
+++ model/user.model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+	emailAddress: { type: String , lowercase: true},
+	password: String,
+	dateOfBirth: String,
+	profile:{
+		firstName: { type: String, lowercase: true , unique: true, required: true },
+		lastName: { type: String, lowercase: true , unique: true, required: true },
+		timestamp: { type : Date, default: Date.now },
+		Avatar: String
+	},
+	status:{
+		lastSeen: String,
+	    active: String
+	},
+	socialMediaSignUp:{
+		facebook: {type: String },
+        twitter: {type: String },
+        instagram: {type: String }},
+	contacts:{
+		list: Array,
+		requested: Array,
+		pending: Array,
+		blocked: Array
+	}
+});
+
+module.exports = mongoose.model('user', userSchema);
