2e72999447e16ca6ff7590e2472d31de77d6c8c1
josearmandojacq <josearmandocastilloquiala@joses-mbp.intern>
2017-11-14 14:35:17 +0100

    adding the getUser API

---
+++ RestFul API/get_users.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const Contact = require('./model/user.model');
+const db = 'mongodb://localhost:27017/skypeClone';
+
+mongoose.Promise = global.Promise;
+mongoose.connection.openUri(db);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
+app.get('/get_users/:id', (request, response)=>{
+    Contact.find({
+        _id: request.params.id
+    }).exec((err, contact)=>{
+        if(err){
+            response.send('We can not find the friends');
+        }else{
+            response.json(contact.contacts.list);
+        }
+    });
+});
+
+
+
+app.listen(8080);
+
