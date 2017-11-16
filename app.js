dbbc68127d7fd43e656af1a641aff4153a636309
Michal Koudelka <koudelka.michal@gmail.com>
2017-11-16 16:32:54 +0100

    Basic auth support

--- app.js
+++ app.js
@@ -4,10 +4,28 @@ const bodyParser = require('body-parser');
 const mongo = require('mongodb');
 const mongoose = require('mongoose');
 
+const passport = require('passport');
+
+const SerialAuthenticator = require('./lib/auth/index');
+
 const Message = require('./lib/models/message.model');
 const User = require('./lib/models/user.model');
 const db = 'mongodb://localhost:27017/skypeClone';
 
+
+const authStrategies = {
+  local : require('./lib/auth/local')(User)
+}
+
+const serialAuthenticator = new SerialAuthenticator(User); // pun intended
+
+app.use(require('express-session')({ secret: "FIXME: I should be retrieved from env var ;(", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(authStrategies.local);
+
+passport.serializeUser=serialAuthenticator.serialize;
+passport.deserializeUser=serialAuthenticator.deserialize;
 
 //Handlers
 
@@ -36,6 +54,7 @@ app.get('/friend/accept/:id', friendHandler.accept.bind(friendHandler))
 app.get('/friend/decline/:id', friendHandler.decline.bind(friendHandler))
 app.get('/friend/remove/:id', friendHandler.remove.bind(friendHandler));
 
+app.get('/login/local', passport.authenticate('local'), (req,res) => { res.send('ok') });
 app.listen(port, function() {
 	console.log('Server started .....')
 });
