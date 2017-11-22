c8aa53c46aca895be8e1943f9b54c8a2d6df0dcd
Yaser <yasser.morteza@gmail.com>
2017-11-22 10:26:17 +0100

    profile rest API updated

--- README.md
+++ README.md
@@ -13,17 +13,11 @@ Building SkypeClone using Material-UI, React.js, Express.js, Node.js, MongoDB &a
 - Mir
 - Michal
 ---
-<<<<<<< HEAD
 
 ## Data Temp
  For the porpuse of the examine the Schema of the mongoose we created the `data.js` file in `temporary` folder.
  -To add the data to your ``Database`` apply the following lines in your mongo database in `Terminal`:
 
-=======
-## Data Temp
- For the porpuse of the examine the Schema of the mongoose we created the `data.js` file in `temporary` folder.
- -To add the data to your ``Database`` apply the following lines in your mongo database:
->>>>>>> 393f9d7422979115d9cda87d516aaf41a753e1ca
   ````
    use skypeClone
    db.createCollection('users')
--- app.js
+++ app.js
@@ -3,16 +3,13 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongo = require('mongodb');
 const mongoose = require('mongoose');
-
 const passport = require('passport');
 
 const SerialAuthenticator = require('./lib/auth/index');
-
 const Message = require('./lib/models/message.model');
 const User = require('./lib/models/user.model');
+
 const db = 'mongodb://localhost:27017/skypeClone';
-
-
 const authStrategies = {
   local : require('./lib/auth/local')(User)
 }
@@ -28,16 +25,17 @@ passport.serializeUser=serialAuthenticator.serialize;
 passport.deserializeUser=serialAuthenticator.deserialize;
 
 //Handlers
-
 const UserHandler = require('./lib/handlers/user.js');
 const FriendHandler = require('./lib/handlers/friend.js');
 const MessageHandler = require('./lib/handlers/message.js');
+const ProfileHandler = require('./lib/handlers/profile.js');
 
 const userHandler = new UserHandler(User);
 const friendHandler = new FriendHandler(User);
 const messageHandler = new MessageHandler(Message);
+const profileHandler = new ProfileHandler(User);
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 mongoose.Promise = global.Promise;
 mongoose.connection.openUri(db);
 
@@ -50,6 +48,9 @@ app.get('/message/get/:id', messageHandler.get.bind(messageHandler));
 app.get('/user/get/:id', userHandler.get.bind(userHandler));
 app.post('/user/register', userHandler.register.bind(userHandler));
 
+app.get('/user/profile/:id', profileHandler.getProfile.bind(profileHandler));
+app.post('/user/profile_edit/:id', profileHandler.editProfile.bind(profileHandler)); 
+
 app.get('/friend/add/:id', friendHandler.add.bind(friendHandler));
 app.get('/friend/accept/:id', friendHandler.accept.bind(friendHandler))
 app.get('/friend/decline/:id', friendHandler.decline.bind(friendHandler))
@@ -57,5 +58,5 @@ app.get('/friend/remove/:id', friendHandler.remove.bind(friendHandler));
 
 app.get('/login/local', passport.authenticate('local'), (req,res) => { res.send('ok') });
 app.listen(port, function() {
-  console.log('Server started .....')
+  console.log('Server started on port.....' + port );
 });
