8d01bfa3916b7fac27cb92eb53a020a7f36476bc
Yaser <yasser.morteza@gmail.com>
2017-12-04 15:47:40 +0100

    Multiparty works as a middleware fine

--- app.js
+++ app.js
@@ -21,7 +21,21 @@ const serialAuthenticator = new SerialAuthenticator(User); // pun intended
 app.use((err,req, res, next)=>{});
 
 app.use(router);
-app.use(multiparty);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false }));
+//app.use(multiparty);
+
+// router.post('/upload/:id', multiparty,(req, res, next)=>{
+//     var name;
+//     console.log(req.filename);
+//     // req.on('end', ()=>{
+//     // 	name = req.filename;
+//     // 	console.log(name);
+
+//     // }); 
+//     //next();
+//     res.send('File uploaded');    
+// });
 
 app.use(require('express-session')({ secret: "FIXME: I should be retrieved from env var ;(", resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
@@ -46,9 +60,6 @@ const port = process.env.PORT || 8080;
 mongoose.Promise = global.Promise;
 mongoose.connection.openUri(db);
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false }));
-
 app.post('/message/send', messageHandler.send.bind(messageHandler));
 app.get('/message/get/:id', messageHandler.get.bind(messageHandler));
 
@@ -56,7 +67,7 @@ app.get('/user/get_friends/:id', userHandler.get.bind(userHandler));
 app.post('/user/register', userHandler.register.bind(userHandler));
 
 app.get('/user/profile/:id', profileHandler.getProfile.bind(profileHandler));
-router.post('/user/profile_edit/:id',profileHandler.editProfile.bind(profileHandler)); 
+app.post('/user/profile_edit/:id',multiparty,profileHandler.editProfile.bind(profileHandler)); 
 
 app.get('/friend/add/:id', friendHandler.add.bind(friendHandler));
 app.get('/friend/accept/:id', friendHandler.accept.bind(friendHandler))
@@ -65,7 +76,7 @@ app.get('/friend/remove/:id', friendHandler.remove.bind(friendHandler));
 
 app.get('/login/local', passport.authenticate('local'), (req,res) => { res.send('ok') });
 
-app.listen(port, function() {
+app.listen(port, ()=>{
   console.log('Server started on port.....' + port );
 });
 
