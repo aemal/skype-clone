9dcc9c3a28413be97f31b8871096da22db39d37a
Ahmad othman <othman0ahmad@gmail.com>
2017-12-19 13:11:49 +0100

    contact detail and new message

--- client/src/components/newMessage.js
+++ client/src/components/newMessage.js
@@ -1,14 +1,54 @@
 import React, { Component } from 'react';
+import { withStyles } from 'material-ui/styles';
+import Input, { InputLabel } from 'material-ui/Input';
+import TextField from 'material-ui/TextField';
+import { FormControl } from 'material-ui/Form';
+import purple from 'material-ui/colors/purple';
+
+const styles = theme => ({
+
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  formControl: {
+    margin: theme.spacing.unit,
+    width:'100%',
+  },
+  inputLabelFocused: {
+    color: purple[500],
+  },
+  inputInkbar: {
+    '&:after': {
+      backgroundColor: purple[500],
+    },
+}
+})
 
 class NewMessage extends Component {
-
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        <p>./components/newMessage.js</p>
+    <div className={classes.container}>
+          <FormControl className={classes.formControl}>
+            <InputLabel
+              FormControlClasses={{
+                focused: classes.inputLabelFocused,
+              }}
+              htmlFor="custom-color-input"
+            >
+              Type your message here..
+            </InputLabel>
+            <Input
+              classes={{
+                inkbar: classes.inputInkbar,
+              }}
+              id="custom-color-input"
+            />
+          </FormControl>
       </div>
     );
   }
 }
 
-export default NewMessage;
+export default withStyles(styles)(NewMessage);
