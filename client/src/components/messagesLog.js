49911a1fe183a7a41d32b5e0b9d4b93299d3c071
Yaser <yasser.morteza@gmail.com>
2017-12-19 14:42:12 +0100

    message log

--- client/src/components/messagesLog.js
+++ client/src/components/messagesLog.js
@@ -1,24 +1,30 @@
 import React, { Component } from 'react';
 import SkypeAvatar from './skypeAvatar';
-import avatar from './avatar.jpg';
+import avatar from './3.jpg';
 
 class MessagesLog extends Component {
-  constructor(){
-  	super();
-  	this.state = {
-  		avatar: avatar
-  	}
-  }
   render() {
     return (
-		<div>
-			<div style={{width:'50%'}}>
-				<SkypeAvatar avatar={avatar} size={50}/>
-			</div>
-			<div style={{width:'50%', float:'right'}}>
-				<SkypeAvatar avatar={avatar} size={50}/>
-			</div>
-		</div>
+        <div>
+          <div>
+            <div style={{border: '1px solid', marginTop:'2%'}}>
+              <div style={{width: '10%', border: '1px solid'}}>
+               <SkypeAvatar avatar={avatar} size={60}/>
+              </div>
+              <div>
+               <p>Hi There!</p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div style={{width:'70%'}}>
+              <p>Hi There!</p>
+            </div>
+            <div style={{float:'right', width: '10%', border: '1px solid'}}>
+             <SkypeAvatar avatar={avatar} size={60}/>
+            </div>
+          </div> 
+        </div>
     );
   }
 }
