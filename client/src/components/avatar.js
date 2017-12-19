f8180200aab5fa941193a07279bf63f1a803f405
josearmandojacq <josearmandocastilloquiala@joses-mbp.intern>
2017-12-19 14:27:54 +0100

    changed the avatar layout

--- client/src/components/avatar.js
+++ client/src/components/avatar.js
@@ -1,26 +1,62 @@
 import React, { Component } from 'react';
+import {withStyles} from 'material-ui/styles';
 import IconButton from 'material-ui/IconButton';
+import Avatar from './skypeAvatar';
+
+const styles = {
+  row:{
+    display: 'flex',
+    justifyContent: 'center',
+    flexWrap: 'wrap'
+  },
+  avatar:{
+    position: 'absolute',
+    bottom: 0,
+    left: '10%'
+  },
+  
+  iconsContainer:{
+    float: 'right',
+  },
+  icons:{
+    height: 60,
+  },
+  img:{
+    width: '100%',
+  }
+  
+};
 
 
-class Avatar extends Component {
+
+class UserAvatar extends Component {
 
   render() {
+     const{classes} = this.props;
     return (
-    <div>
-      <div className="avatarIcons">
-        <IconButton tooltip="Font Icon">
-          <i className="material-icons">home</i>
+      <div >
+        <div className={classes.name}>
+          
+        </div>
+        <div className={classes.iconsContainer}>
+        <IconButton tooltip="SVG Icon" className={classes.icons}>
+          <i class="material-icons">settings</i>
         </IconButton>
-        <IconButton tooltip="SVG Icon">
-           <i className="material-icons">settings</i>
-         </IconButton>
-      </div>
-      <div className="avatarPhoto">
-      </div>
-      <p>./components/avatar.js</p>
+        <IconButton className={classes.icons}>
+          <i class="material-icons">notifications</i>
+        </IconButton>
+        <IconButton tooltip="Font Icon" className={classes.icons}>
+          <i class="material-icons">exit_to_app</i>
+        </IconButton>
+        </div>
+        <div className={classes.avatar}>
+          <Avatar avatar={'http://cdn.skim.gs/images/c_fill,dpr_1.0,f_auto,fl_lossy,h_391,q_auto,w_695/funny-dog-names/funny-dog-names'} size={100} name={'Yasser'}/>
+        </div>
     </div>
+      
+    
     );
   }
 }
 
-export default Avatar;
+export default withStyles(styles)(UserAvatar);
