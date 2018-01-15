613f4d3a697d3d84fa9a614e42bbf6cf24eb42f8
sareh88 <almaazsareh@gmail.com>
2018-01-15 14:45:11 +0100

    install  react router redux in the skypeClone

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import contactListReducers from './contactListReducers';
 import contactListFilterReducer from './contactListFilterReducer';
+import { routerReducer } from 'react-router-redux'
 
 export default combineReducers({
 	contactListReducers,
-	contactListFilterReducer
+	contactListFilterReducer,
+	router: routerReducer
 }) 
--- src/store.js
+++ src/store.js
@@ -1,8 +1,13 @@
 import {applyMiddleware, createStore} from 'redux';
-
+//import createHistory from 'history/createBrowserHistory';
 import thunk from 'redux-thunk';
 import reducer from './reducers';
+//import { ConnectedRouter, routerReducer, routerMiddleware, push } from 'react-router-redux';
+
+//const history = createHistory();
+//const Routemiddleware = routerMiddleware(history)
 
 const middlewares= applyMiddleware( thunk);
+
 
 export default createStore(reducer,middlewares);
