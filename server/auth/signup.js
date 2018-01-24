38d1fa4c6fe3699423e2ab3b5172ee0daf4e6c6d
Malos <yasser.morteza@gmail.com>
2018-01-24 15:23:53 +0100

    signup changed to normal function and it works fine, the error handling is fixed

--- server/auth/signup.js
+++ server/auth/signup.js
@@ -1,56 +1,55 @@
 const bcrypt = require('bcryptjs');
-const LocalStrategy = require('passport-local').Strategy;
 
-module.exports = (userModel, passport) => {
-    passport.use('signup', new LocalStrategy({
-            passReqToCallback: true
-        },
-        (req, username, password, done) => {
-            userModel.findOne({
-                'emailAddress': username
+module.exports = class {
+    constructor(userModel){
+        this.userModel = userModel;
+    };
+    
+    signup(req, res, next){
+            this.userModel.findOne({
+                'emailAddress': req.username
             }, (err, user) => {
                 if (err) {
-                    return done(err);
+                    return next(err);
                 }
                 if (user) {
-                    return done(null, false, console.log('User already exists'));
+                    return res.send({ success : false, message : 'Singin failed, email is already exist' });;
                 } else {
-                    // create the user
-                    bcrypt.genSalt(10, (err, salt) => {
-                        bcrypt.hash(req.body.password, salt, (err, hash) => {
-                            var user = new userModel({
-                                emailAddress: req.body.username,
-                                password: hash,
-                                dateOfBirth: new Date(req.body.dateOfBirth),
-                                profile: {
-                                    firstName: req.body.firstName,
-                                    lastName: req.body.lastName,
-                                    gender: req.body.gender,
-                                    avatarURL: 'avatar.jpg'
-                                },
-                                loginStrategy: 'signin',
-                                loginObject: {}, //We do not really need it for local strateggy so far 
-                                contacts : {
-                                      decline : [ ],
-                                      pending : [ ],
-                                      requested : [ ],
-                                      blocked : [ ],
-                                      friends : [ ]
-                                    }
-                            });
-                            try {
-                                user.save((err) => {
-                                    if (err) throw (err);
-                                    console.log('User is created');    
-                                    return done(null, user);
+                    let password = req.body.password;
+                    if(password.length >= 8 && password.length <= 20){
+                        // create the user
+                        bcrypt.genSalt(10, (err, salt) => {
+                            bcrypt.hash(req.body.password, salt, (err, hash) => {
+                                let user = new this.userModel({
+                                        emailAddress: req.body.username,
+                                        password: hash,
+                                        dateOfBirth: new Date(req.body.dateOfBirth),
+                                        profile: {
+                                            firstName: req.body.firstName,
+                                            lastName: req.body.lastName,
+                                            gender: req.body.gender,
+                                            avatarURL: 'avatar.jpg'
+                                        },
+                                        loginStrategy: 'signin'
                                 });
-                            } catch (err) {
-                                //TODO We should be able to handle different kind of errors and send an appropriate error message
-                                console.log(err);
-                            }
-                        });
-                    });
+                                try {
+                                    user.save((err) => {
+                                        if (err){ 
+                                            return next(err)
+                                        }else{;
+                                            return res.send(user)
+                                        };
+                                    });
+                                } catch (err) {
+                                    return next(err);
+                                }
+                            });
+                        });   
+                    }else{
+                        return res.send({ success : false, message : 'Password is min 8 and max 20 characters' });;
+                    }
+                    
                 }
             });
-        }));
+        };
 }
