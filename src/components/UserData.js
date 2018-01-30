7ed6a0e34158850624a0574b3127af1f08d83f86
josearmandojacq <castilloquiala@campus.tu-berlin.de>
2018-01-30 21:03:35 +0100

    updated the isRequired function

--- src/components/UserData.js
+++ src/components/UserData.js
@@ -7,9 +7,9 @@ import { FormLabel, FormControl, FormControlLabel } from "material-ui/Form";
 import { DatePicker } from "material-ui-pickers";
 import Typography from "material-ui/Typography/Typography";
 import moment from "moment";
-import {signup} from '../actions/signup';
-import {connect} from 'react-redux';
-import Avatar from './skypeAvatar';
+import { signup } from "../actions/signup";
+import { connect } from "react-redux";
+import Avatar from "./skypeAvatar";
 
 const styles = theme => ({
   container: {
@@ -70,7 +70,7 @@ const styles = theme => ({
     marginBottom: 30,
     width: 200
   },
-  text:{
+  text: {
     color: "black"
   }
 });
@@ -90,7 +90,7 @@ class UserData extends Component {
         password: "******",
         newPassword: "newPasssword"
       },
-      errorMessage:' '
+      errorMessage: " "
       // firstNameRequired:false,
       // lastNameRequired:false,
       // emailRequired:false,
@@ -100,9 +100,9 @@ class UserData extends Component {
       // repasswordRequired:false,
       // dateOfBirthRequird:false,
       // genderRequired:false
-    } 
+    };
     this.handleInputChange = this.handleInputChange.bind(this);
-    this.isRequierd = this.isRequierd.bind(this)
+    this.isRequierd = this.isRequierd.bind(this);
   }
 
   handleChange = (event, value) => {
@@ -110,80 +110,86 @@ class UserData extends Component {
   };
 
   handleDataChange = date => {
-    
     let checketDate = date.format().substring(0, 10);
     console.log("date", checketDate);
     this.setState({
-      newUser:{...this.state.newUser,dateOfBirth:checketDate}
+      newUser: { ...this.state.newUser, dateOfBirth: checketDate }
     });
   };
 
-  isRequierd(formData){
-     if(formData === undefined){
-       return false
-     }
-     if(formData.firstName || formData.lastName || formData.formDataemail || formData.password || formData.newPassword || formData.dateOfBirth === ' ') {
-       return false;
-     }else {
-       return true;
-     }
+  isRequierd(formData) {
+    if (formData === undefined) {
+      return false;
+    }
+    if (
+      formData.firstName === " " ||
+      formData.lastName === " " ||
+      formData.formDataemail === " " ||
+      formData.password === " " ||
+      formData.newPassword === " " ||
+      formData.dateOfBirth === " "
+    ) {
+      return false;
+    } else {
+      return true;
+    }
   }
-  
-  handleInputChange(event){
+
+  handleInputChange(event) {
     const target = event.target;
     const value = target.value;
     const name = target.name;
-  
+
     this.setState({
-      newUser:{
+      newUser: {
         ...this.state.newUser,
-        [name]:value
+        [name]: value
       }
     });
- 
   }
-  handleSubmit(e){
+  handleSubmit(e) {
     e.preventDefault();
-    console.log(this.state.newUser)
+    console.log(this.state.newUser);
     let formData = this.state.newUser;
     let url = "http://localhost:3001/auth/signup";
 
-    if(this.isRequierd(formData)){
-      
-        const searchParams = Object.keys(formData).map((key) => {
-          return encodeURIComponent(key) + '=' + encodeURIComponent(formData[key]);
-        }).join('&');
-        console.log(searchParams);
-        fetch(url, {
-          CORS:'disabled',
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-          },
-          body: searchParams
+    console.log(this.isRequierd(formData));
+    if (this.isRequierd(formData)) {
+      const searchParams = Object.keys(formData)
+        .map(key => {
+          return (
+            encodeURIComponent(key) + "=" + encodeURIComponent(formData[key])
+          );
         })
-        .then(res=>res.json())
-        .then( data =>{
+        .join("&");
+      console.log(searchParams);
+      fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
+        },
+        body: searchParams
+      })
+        .then(res => res.json())
+        .then(data => {
           console.dir(data);
-          if(data.success){
-            this.props.history.push('/wellcome')
-          }else{
+          if (data.success) {
+            this.props.history.push("/wellcome");
+          } else {
             this.setState({
-              error:data.message
-            })
-            console.log(this.state.error)
+              error: data.message
+            });
+            console.log(this.state.error);
           }
-          
-          
         })
-        .catch(err=>console.log(err));
-    }else{
+        .catch(err => console.log(err));
+    } else {
       this.setState({
-        errorMessage:'please fill the Required filed'
-      })//Handle errors here...
-    } 
+        errorMessage: "please fill the Required filed"
+      }); //Handle errors here...
+    }
   }
-   
+
   componentWillMount() {
     if (this.props.place === "setting") {
       this.setState({
@@ -201,94 +207,119 @@ class UserData extends Component {
         }
       });
     }
-  }  
-    
-    render(){
-        const {classes} = this.props;
+  }
 
-        return(
-          <div className="main-container">
-          <div className={classes.avatar}>
-            <Avatar avatar={'https://cdn3.iconfinder.com/data/icons/avatars-9/145/Avatar_Penguin-512.png'} size={80} />
-          </div>
-            <h3 className="sign-in-header">Sign up</h3>
-            <div className="sign-in-details">
-            <form className={classes.formWrapper} noValidate autoComplete='off' onSubmit={this.handleSubmit.bind(this)}>
-                <TextField 
-                    id='firstName'
-                    className={classes.textField}
-                    label={this.state.settingUserData.firstName}
-                    onChange={this.handleInputChange}
-                    name='firstName'
-                  
-                />
+  render() {
+    const { classes } = this.props;
 
-                <TextField
-                    id='lastName'
-                    className={classes.textField}
-                    label={this.state.settingUserData.lastName}
-                    onChange={this.handleInputChange}
-                    name='lastName'
-                />
-                
-                <TextField
-                    id='email'
-                    className={classes.textField}
-                    label={this.state.settingUserData.email}
-                    onChange={this.handleInputChange}
-                    name='email'
-                    
-                />
-                <TextField
-                    id='password'
-                    className={classes.textField}
-                    label={this.state.settingUserData.password}
-                    onChange={this.handleInputChange}
-                    name='password'
-                />
-                <TextField 
-                    id='newPassword'
-                    className={classes.textField}
-                    label={this.state.settingUserData.newPassword}
-                    onChange={this.handleInputChange}
-                    name='newPassword'
-                />
-                <div className='picker'>
-                    <Typography type='caption' align='left' gutterBottom className={classes.Typography} >
-                        Date of Birth
-                    </Typography>
-                    <DatePicker
-                        keyboard
-                        value={this.state.selectedDate}
-                        labelFunc={date => moment(date).format('Do MMMM YYYY')}
-                        onChange={this.handleDataChange}
-                        className={classes.DatePicker}
-                    />
-                </div>
-                <FormControl component='fieldset' className={classes.FormControl}>
-                    <FormLabel component='legend'>Gender</FormLabel>
-                    <RadioGroup
-                        aria-label='gender'
-                        value={this.state.value}
-                        onChange={this.handleChange}
-                        className={classes.RadioGroup}
-                    >
-                        <FormControlLabel value='male' control={<Radio />} label='Male' />
-                        <FormControlLabel value='female' control={<Radio />} label='Female' />
-                        <FormControlLabel value='other' control={<Radio />} label='Other' />
-                    </RadioGroup>
-                </FormControl>
-                <Button type="submit" className="login-button">
-                    {this.state.buttonTitle}
-                </Button>
-                <div><p className={classes.text}>{this.state.errorMessage}</p></div>
-              </form>
+    return (
+      <div className="main-container">
+        <div className={classes.avatar}>
+          <Avatar
+            avatar={
+              "https://cdn3.iconfinder.com/data/icons/avatars-9/145/Avatar_Penguin-512.png"
+            }
+            size={80}
+          />
+        </div>
+        <h3 className="sign-in-header">Sign up</h3>
+        <div className="sign-in-details">
+          <form
+            className={classes.formWrapper}
+            noValidate
+            autoComplete="off"
+            onSubmit={this.handleSubmit.bind(this)}
+          >
+            <TextField
+              id="firstName"
+              className={classes.textField}
+              label={this.state.settingUserData.firstName}
+              onChange={this.handleInputChange}
+              name="firstName"
+            />
+
+            <TextField
+              id="lastName"
+              className={classes.textField}
+              label={this.state.settingUserData.lastName}
+              onChange={this.handleInputChange}
+              name="lastName"
+            />
+
+            <TextField
+              id="email"
+              className={classes.textField}
+              label={this.state.settingUserData.email}
+              onChange={this.handleInputChange}
+              name="email"
+            />
+            <TextField
+              id="password"
+              className={classes.textField}
+              label={this.state.settingUserData.password}
+              onChange={this.handleInputChange}
+              name="password"
+            />
+            <TextField
+              id="newPassword"
+              className={classes.textField}
+              label={this.state.settingUserData.newPassword}
+              onChange={this.handleInputChange}
+              name="newPassword"
+            />
+            <div className="picker">
+              <Typography
+                type="caption"
+                align="left"
+                gutterBottom
+                className={classes.Typography}
+              >
+                Date of Birth
+              </Typography>
+              <DatePicker
+                keyboard
+                value={this.state.selectedDate}
+                labelFunc={date => moment(date).format("Do MMMM YYYY")}
+                onChange={this.handleDataChange}
+                className={classes.DatePicker}
+              />
             </div>
-            
-          </div>
-        );
-      }
-    }
-      
-      
-export default withStyles(styles)(connect(null,{signup})(UserData));
+            <FormControl component="fieldset" className={classes.FormControl}>
+              <FormLabel component="legend">Gender</FormLabel>
+              <RadioGroup
+                aria-label="gender"
+                value={this.state.value}
+                onChange={this.handleChange}
+                className={classes.RadioGroup}
+              >
+                <FormControlLabel
+                  value="male"
+                  control={<Radio />}
+                  label="Male"
+                />
+                <FormControlLabel
+                  value="female"
+                  control={<Radio />}
+                  label="Female"
+                />
+                <FormControlLabel
+                  value="other"
+                  control={<Radio />}
+                  label="Other"
+                />
+              </RadioGroup>
+            </FormControl>
+            <Button type="submit" className="login-button">
+              {this.state.buttonTitle}
+            </Button>
+            <div>
+              <p className={classes.text}>{this.state.errorMessage}</p>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(connect(null, { signup })(UserData));
