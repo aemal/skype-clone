9912def8081cbc48df7df6884adf5b54d1509f17
Malos <yasser.morteza@gmail.com>
2018-02-02 20:37:04 +0100

    edited to update db and auth tested both work fine

--- server/handlers/profile.js
+++ server/handlers/profile.js
@@ -1,5 +1,7 @@
-const multiparty = require('./file-upload');
+'use strict';
+
 const bcrypt = require('bcryptjs');
+
 module.exports = class {
   constructor(userModel) {
     this.userModel = userModel;
@@ -8,63 +10,80 @@ module.exports = class {
   getProfile(req,res) {
     this.userModel.findOne({_id: req.params.id}).exec((err, user)=>{
     if(err||!user){
-          res.sendStatus(404);
+          res.json({ success : false, message : 'User not found' });
       }else{
-          res.json(user.profile);
+          let {emailAddress, profile, gender, dateOfBirth, status, _id, accessToken} = user;
+          res.json({emailAddress, profile, gender, dateOfBirth, status});
       }
     });
   }
   
-  updatePassword(req,res){
-    this.userModel.findById(req.params.id,(err, user)=>{
-      if(err){
-        res.sendStatus(500);
-      }else{
-        bcrypt.genSalt(10,(err, salt)=>{
-          bcrypt.hash(req.body.newPassword, salt, (err, hash)=>{
-            user.password = hash;
-          });
-        })
-      }
-      try {
-        user.save((err) => {
-          if (err) throw(err);
-          res.status(200).send(user);
-        });
-      } catch(err) {
-        console.log(err);
-        res.sendStatus(400);
-      }
-
-    });
-   }
+  updatePassword(req,res,next){
+    let id = req.params.id;
+    let password = req.body.password;
+    if(password.length >= 8 && password.length <= 20){
+        bcrypt.genSalt(10,(err, salt)=>bcrypt.hash(password, salt, (err, hash)=>{
+            this.userModel.findOneAndUpdate({_id: id},
+                        {
+                         $set:{
+                           password: hash
+                          }
+                        },{upsert: false}, (err, user)=>{
+                                 if(err) return next(err);
+                                 else return res.json({ success : true, message : 'Password is changed successfully' });
+            });
+        }));
+    }else{
+        return res.json({ success : false, message : 'Password is min 8 and max 20 characters' });
+    }
+  };
 
   editProfile(req,res, next) {
-        this.userModel.findById(req.params.id,(err, user)=>{
-          if(err){
-              res.sendStatus(500);
-          }else{
-              user.emailAddress = req.body.emailAddress || user.emailAddress,
-              user.password = hash || user.password,
-              user.dateOfBirth = new Date(req.body.dateOfBirth) || user.dateOfBirth,
-              user.profile = {
-                              firstName : req.body.firstName || user.profile.firstName,
-                              lastName : req.body.lastName || user.profile.lastName,
-                              gender: req.body.gender || user.profile.gender,
-                              avatarURL : req.filename || user.profile.avatarURL
-                            }
-          }
-          try {
-            user.save((err) => {
-              if (err) throw(err);
-              res.status(200).send(user);
+        let id = req.params.id,
+            user = req.user,
+            body = req.body,
+            emailAddress = body.emailAddress.toLowerCase() || user.emailAddress,
+            dateOfBirth = new Date(body.dateOfBirth) || user.dateOfBirth,
+            profile = {
+                       firstName: body.firstName || user.profile.firstName,
+                       lastName: body.lastName || user.profile.lastName,
+                       gender: body.gender || user.profile.gender,
+                       avatarURL: req.filename || user.profile.avatarURL
+                     };
+        if(body.emailAddress){
+            this.userModel.findOne({
+                'emailAddress': req.body.emailAddress.toLowerCase()
+            }, (err, user) => {
+                if (err)return next(err);
+                if (user) {
+                    return res.json({ success : false, message : 'This email is already exist' });
+                } else {
+                    this.userModel.findOneAndUpdate({_id: id},
+                                    {
+                                     $set:{profile : profile,
+                                          emailAddress : emailAddress,
+                                          dateOfBirth : dateOfBirth
+                                      }
+                                    },{upsert: false ,multi: true}, (err, user)=>{
+                                             if(err) return next(err);
+                                             else return res.json({ success : true, message : 'profile is edited successfully' });
+                    });
+                }
+              }
+            );    
+        }else{
+            this.userModel.findOneAndUpdate({_id: id},
+                            {
+                             $set:{profile : profile,
+                                  dateOfBirth : dateOfBirth
+                              }
+                            },{upsert: false ,multi: true}, (err, user)=>{
+                                     if(err) return next(err);
+                                     else return res.json({ success : true, message : 'profile is edited successfully' });
             });
-          } catch(err) {
-            console.log(err);
-            res.sendStatus(400);
-          }
-        });
-     
+        }
         
-    }
-}
+  }
+};
+
+
