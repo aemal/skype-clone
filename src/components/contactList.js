cb0b5aafa5dc7004667a01ad83dcfd09f8015349
sareh88 <almaazsareh@gmail.com>
2018-02-06 20:34:02 +0100

    socket setting

--- src/components/contactList.js
+++ src/components/contactList.js
@@ -3,7 +3,7 @@ import { withStyles } from "material-ui/styles";
 import List, { ListItem, ListItemText } from "material-ui/List";
 import SkypeAvatar from "./skypeAvatar";
 import SearchList from "../components/searchList";
-
+import decode from 'jwt-decode';
 const styles = theme => ({
   root: {
     width: "100%"
@@ -11,12 +11,31 @@ const styles = theme => ({
 });
 
 class ContactList extends Component {
+  constructor(){
+    super()
+    this.state={
+      socketChanelId:''
+    }
+  }
+  socketChanel(friendId){
+   let user = decode(localStorage.getItem('token')) ;
+   let userId = user._id;
+   let socketChanelId = userId+friendId
+   console.log(socketChanelId)
+   this.setState({
+    socketChanelId:socketChanelId
+   })
+   this.props.getId(socketChanelId)
+
+  }
   render() {
     const { classes } = this.props;
     const listItems = this.props.friendsList.map(item => {
       console.log(item);
       return (
-        <ListItem key={item.userId} dense button className="list-item">
+        <ListItem key={item.userId}
+          dense button className="list-item"
+           onClick={() => this.socketChanel(item.userId) }>
           <SkypeAvatar
             avatar={
               // the image will be item.avatarURL
