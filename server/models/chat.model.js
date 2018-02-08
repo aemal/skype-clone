f4cbea642c832285b7107190cdbe7b14166fcea9
yassermorteza <yasser.morteza@gmail.com>
2018-02-08 19:33:58 +0100

    chat handle messages

--- server/models/chat.model.js
+++ server/models/chat.model.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var ChatSchema = new Schema ({
-  participants: [{type: Schema.Types.ObjectId, ref: 'User'}]
+  participants: {type: String, ref: 'User'}
 });
 
 module.exports = mongoose.model('chat', ChatSchema);
