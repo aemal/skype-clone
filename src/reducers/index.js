5e950c6a7e850c98fd30d8a9d6db4261737efc43
Aemal Sayer <aemalsayer@aemals-air.intern>
2018-02-09 15:56:34 +0100

    added select current friend feature

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
 import contactListReducers from './contactListReducers';
 import contactListFilterReducer from './contactListFilterReducer';
+import setCurrentFriendReducer from './setCurrentFriendReducer';
 import { routerReducer } from 'react-router-redux'
 
 export default combineReducers({
 	contactListReducers,
+	setCurrentFriendReducer,
 	contactListFilterReducer,
 	router: routerReducer
 }) 
