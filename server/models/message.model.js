6e73096e002ffd2d8a5df2c0344ca927d43172b1
yassermorteza <yasser.morteza@gmail.com>
2018-02-11 14:43:48 +0100

    message model schema updated for 2 person's chat

--- server/models/message.model.js
+++ server/models/message.model.js
@@ -3,8 +3,12 @@ var Schema = mongoose.Schema;
 
 var MessageSchema = new Schema ({
 	  roomID: {type: Schema.Types.ObjectId, ref: 'Chat'},
-	  userID: {type: Schema.Types.ObjectId, ref: 'User'},
-	  message: String
+	  messages: [
+	  		{
+	  			userID:{type: Schema.Types.ObjectId, ref: 'User'},
+	  			message:String
+	  		}
+	  ]
 });
 
 module.exports = mongoose.model('message', MessageSchema);
