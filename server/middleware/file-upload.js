6189e18606adf7b6f3a4ab06419f8e6845ba79d3
yassermorteza <yasser.morteza@gmail.com>
2018-02-11 14:45:10 +0100

    still on proccess

--- server/middleware/file-upload.js
+++ server/middleware/file-upload.js
@@ -34,7 +34,7 @@ function incomingForm(req, res, next) {
     form.maxFilesSize = 1 * 1024 * 1024;
 
     form.on('error', (err)=>{
-        res.header('Connection', 'close');
+        //res.header('Connection', 'close');
         if(req.filename){
         	fs.unlink(`${form.uploadDir}/${req.filename}`, err=>{if(err) return netx(err);});
         }
@@ -57,8 +57,8 @@ function incomingForm(req, res, next) {
         };
 
         if (part.filename && type === 'image/jpeg' || type === 'image/jpg' || type === 'image/png' || type === 'image/gif') {
-
-            const name = uuidv1() + '-' + Date.now() + '-' + part.filename;
+            const mimeType = type.substring('6');
+            const name = uuidv1() + '-' + Date.now() + '.' + mimeType;
             const path = form.uploadDir + "/" + name;
             req.filename = name;
             part.pipe(fs.createWriteStream(path));
