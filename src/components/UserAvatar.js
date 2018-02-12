74f30efd71b5ca8496566fd8665fc62871deab6e
Ahmad othman <othman0ahmad@gmail.com>
2018-02-12 14:11:00 +0100

    logout works fine

--- src/components/UserAvatar.js
+++ src/components/UserAvatar.js
@@ -5,7 +5,7 @@ import Avatar from "./skypeAvatar";
 import ProfileSettings from "./ProfileSettings";
 import FormDialog from "./dialog";
 import config from "../config/config.js";
-//import { Route, Redirect } from 'react-router'
+import { Route, Redirect } from 'react-router'
 
 
 const styles = {
@@ -27,7 +27,8 @@ const styles = {
 
 class UserAvatar extends Component {
   state = {
-    open: false
+    open: false,
+    redirect : false
   };
 
   handleClickOpen = () => {
@@ -40,55 +41,57 @@ class UserAvatar extends Component {
   };
 
   logOut = () => {
-    //console.log('works');
     let token = localStorage.getItem("token");
     let url = `${config.BASE_URL}auth/logout`;
+
 
     fetch(url, {
       method: "Get",
       headers: {
         Authorization: `TOKEN ${token}`,
-        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
       }
     })
       .then(res => res.json())
       .then(data => {
-         //console.log(data );
-         //<Redirect to="/auth/login"/>
+         localStorage.clear();
+         this.setState({ redirect : true })
       })
       .catch(err => console.log(err));
-
-      console.log({ Error: "Fields are required" }); //Handle errors here...
 
   }
 
 
   render() {
     const { classes } = this.props;
+    console.log(this.state.redirect);
+    if(this.state.redirect){
+      return <Redirect to='/'/>
+    }else{
+      return (
+        <div className={classes.root}>
+          <div className="icons" style={{padding: 0, color: "#fff"}}>
+            <IconButton onClick={this.handleClickOpen} style={{zIndex:1}}>
+              <i className="material-icons" style={{color: "#fff"}}>settings</i>
+            </IconButton>
+            <FormDialog
+              open={this.state.open}
+              handleClose={this.handleClose}
+              compo={<ProfileSettings />}
+              fullScreen={true}
+            />
 
-    return (
-      <div className={classes.root}>
-        <div className="icons" style={{padding: 0, color: "#fff"}}>
-          <IconButton onClick={this.handleClickOpen} style={{zIndex:1}}>
-            <i className="material-icons" style={{color: "#fff"}}>settings</i>
-          </IconButton>
-          <FormDialog
-            open={this.state.open}
-            handleClose={this.handleClose}
-            compo={<ProfileSettings />}
-            fullScreen={true}
-          />
+            <IconButton onClick={this.logOut} style={{zIndex:1}}>
+              <i className="material-icons" style={{color: "#fff"}}>exit_to_app</i>
+            </IconButton>
+          </div>
+          <div className={classes.avatar} style={{padding: 10, marginTop: -60}}>
+            <Avatar size="100px" avatar={this.props.avatarURL} />
+          </div>
 
-          <IconButton onClick={this.logOut} style={{zIndex:1}}>
-            <i className="material-icons" style={{color: "#fff"}}>exit_to_app</i>
-          </IconButton>
         </div>
-        <div className={classes.avatar} style={{padding: 10, marginTop: -60}}>
-          <Avatar size="100px" avatar={this.props.avatarURL} />
-        </div>
+      );
+    }
 
-      </div>
-    );
   }
 }
 
