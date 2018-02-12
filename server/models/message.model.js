45e3c306606045f9e327f22e6468cfce4da8c257
yassermorteza <yasser.morteza@gmail.com>
2018-02-12 12:05:20 +0100

    message limit and save from socket

--- server/models/message.model.js
+++ server/models/message.model.js
@@ -2,13 +2,10 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var MessageSchema = new Schema ({
-	  roomID: {type: Schema.Types.ObjectId, ref: 'Chat'},
-	  messages: [
-	  		{
-	  			userID:{type: Schema.Types.ObjectId, ref: 'User'},
-	  			message:String
-	  		}
-	  ]
+		chatID: {type: Schema.Types.ObjectId, ref: 'Chat'},
+		userID:{type: Schema.Types.ObjectId, ref: 'User'},
+		message:String
+
 });
 
 module.exports = mongoose.model('message', MessageSchema);
