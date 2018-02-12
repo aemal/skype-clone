8a681bfafd7dc7376ae08305539c03344a0f4ed1
sareh88 <almaazsareh@gmail.com>
2018-02-12 11:53:56 +0100

    profilesetting

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require('express');
+/* const express = require('express');
 
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-
+ */
 /*const server = app.listen(8080);
 
 const io = require('socket.io').listen(server);
