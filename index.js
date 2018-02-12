85e977a553275b40b05b4386b47a5e288fb9dc06
yassermorteza <yasser.morteza@gmail.com>
2018-02-12 12:09:29 +0100

    commented all useless and conflicted codes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express');
+// const express = require('express');
 
-const app = express();
+// const app = express();
 
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
 
 
 /*const server = app.listen(8080);
